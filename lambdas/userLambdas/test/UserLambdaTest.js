45a7314b4dd5b75bbed4627c86614fcdfb7feeeb
Christian Cygnus <gamer00@att.net>
2018-03-03 22:15:23 -0600

    Added all relevant tests for current iteration of the user lambda

--- lambdas/userLambdas/test/UserLambdaTest.js
+++ lambdas/userLambdas/test/UserLambdaTest.js
@@ -21,12 +21,12 @@ AWS.mock('DynamoDB.DocumentClient', 'get', function (params, callback) {
 
 
 describe("Test suite for User Lambda", function () {
-    it("Test get item from database", function () {
+    it("Test get user from database", function () {
         let event = {
             httpMethod: "GET",
             body: "{}",
             queryStringParameters: {
-                email: "testPerson@gmail.com"
+                email: "joe.smitty@gmail.com"
             }
         };
 
@@ -39,6 +39,60 @@ describe("Test suite for User Lambda", function () {
 
             assert.equal(body.email, "testPerson@gmail.com");
             assert.equal(body.name, "Test Person");
+        }
+
+    });
+
+    it("Test update user from database", function () {
+        let event = {
+            httpMethod: "PUT",
+            body: "{\"email\":\"joe.smitty@gmail.com\",\"name\":\"Joe Smitty\",\"rating\":\"5\"}"
+        };
+
+        handler(event, null, callback);
+
+        function callback(nothing, response) {
+            assert.equal(response.statusCode, 200);
+
+            let body = JSON.parse(response.body);
+
+            assert.equal(body, "Successfully updated user!");
+        }
+
+    });
+
+    it("Test delete user from database", function () {
+        let event = {
+            httpMethod: "DELETE",
+            body: "{\"email\":\"joe.smitty@gmail.com\"}"
+        };
+
+        handler(event, null, callback);
+
+        function callback(nothing, response) {
+            assert.equal(response.statusCode, 200);
+
+            let body = JSON.parse(response.body);
+
+            assert.equal(body, "Successfully deleted user!");
+        }
+
+    });
+
+    it("Test post user into database", function () {
+        let event = {
+            httpMethod: "POST",
+            body: "{\"email\":\"joe.smitty@gmail.com\",\"name\":\"Joe Smitty\"}"
+        };
+
+        handler(event, null, callback);
+
+        function callback(nothing, response) {
+            assert.equal(response.statusCode, 200);
+
+            let body = JSON.parse(response.body);
+
+            assert.equal(body, "Successfully created user!");
         }
 
     });
