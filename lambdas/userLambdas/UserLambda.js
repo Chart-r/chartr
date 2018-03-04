9a044efb92d95d8ff9bb2e647a414bf0b14250fc
Christian Cygnus <gamer00@att.net>
2018-03-04 00:00:45 -0600

    Added ESLint to Node project

--- lambdas/userLambdas/UserLambda.js
+++ lambdas/userLambdas/UserLambda.js
@@ -3,36 +3,45 @@
 console.log('Loading event');
 let AWS = require('aws-sdk');
 
+/**
+ * Handles a user PUT request using the given doc client.
+ * Expects correct formatting of the body of the
+ * request to directly pass into the Database
+ *
+ * @param {object} docClient Initialized Document Client
+ * @param {object} event Event from the API invocation (JSON)
+ * @param {function} callback Callback for returning from the lambda
+ */
 function handleUserPut(docClient, event, callback) {
     let bodyJson = JSON.parse(event.body);
-    console.log("Body received: ", bodyJson);
+    console.log('Body received: ', bodyJson);
 
-    let expression = "";
+    let expression = '';
     let valueObject = {};
     let nameObject = {};
 
     Object.keys(bodyJson).forEach((key) => {
-        if (key.toString() !== "email") {
-            let valueKey = ":" + Math.random().toString(36).substring(7);
-            let nameKey = "#" + Math.random().toString(36).substring(7);
+        if (key.toString() !== 'email') {
+            let valueKey = ':' + Math.random().toString(36).substring(7);
+            let nameKey = '#' + Math.random().toString(36).substring(7);
 
-            expression += nameKey + " = " + valueKey + ", ";
+            expression += nameKey + ' = ' + valueKey + ', ';
 
             valueObject[valueKey] = bodyJson[key];
             nameObject[nameKey] = key.toString();
         }
     });
 
-    expression = "set " + expression.substring(0, expression.length - 2);
+    expression = 'set ' + expression.substring(0, expression.length - 2);
 
     let params = {
-        TableName: "User",
+        TableName: 'User',
         Key: {
-            "email": bodyJson["email"]
+            'email': bodyJson['email'],
         },
         UpdateExpression: expression,
         ExpressionAttributeValues: valueObject,
-        ExpressionAttributeNames: nameObject
+        ExpressionAttributeNames: nameObject,
     };
 
     console.log(expression);
@@ -43,118 +52,151 @@ function handleUserPut(docClient, event, callback) {
         if (err) {
             console.log(err);
 
-            writeAPIOutput(500, "Server error", callback);
-        }
-        else {
+            writeAPIOutput(500, 'Server error', callback);
+        } else {
             console.log(data);
 
-            writeAPIOutput(200, "Successfully updated user!", callback);
+            writeAPIOutput(200, 'Successfully updated user!', callback);
         }
     });
 }
 
+/**
+ * Handles a user DELETE request using the given doc client.
+ * Expects correct formatting of the body of the
+ * request to directly pass into the Database
+ *
+ * @param {object} docClient Initialized Document Client
+ * @param {object} event Event from the API invocation (JSON)
+ * @param {function} callback Callback for returning from the lambda
+ */
 function handleUserDelete(docClient, event, callback) {
     let bodyJson = JSON.parse(event.body);
-    console.log("Body received: ", bodyJson);
+    console.log('Body received: ', bodyJson);
 
     let params = {
-        TableName: "User",
-        Key: bodyJson
+        TableName: 'User',
+        Key: bodyJson,
     };
 
-    docClient.delete(params, function (err, data) {
+    docClient.delete(params, function(err, data) {
         if (err) {
             console.log(err);
 
-            writeAPIOutput(500, "Server error", callback);
-        }
-        else {
+            writeAPIOutput(500, 'Server error', callback);
+        } else {
             console.log(data);
 
-            writeAPIOutput(200, "Successfully deleted user!", callback);
+            writeAPIOutput(200, 'Successfully deleted user!', callback);
         }
     });
 }
 
+/**
+ * Handles a user GET request using the given doc client.
+ * Expects correct formatting of the body of the
+ * request to directly pass into the Database
+ *
+ * @param {object} docClient Initialized Document Client
+ * @param {object} event Event from the API invocation (JSON)
+ * @param {function} callback Callback for returning from the lambda
+ */
 function handleUserGet(docClient, event, callback) {
-    let bodyJson = event["queryStringParameters"];
-    console.log("Body received: ", bodyJson);
+    let bodyJson = event['queryStringParameters'];
+    console.log('Body received: ', bodyJson);
 
     let params = {
-        TableName: "User",
-        Key: bodyJson
+        TableName: 'User',
+        Key: bodyJson,
     };
 
-    docClient.get(params, function (err, data) {
+    docClient.get(params, function(err, data) {
         if (err) {
             console.log(err);
 
             writeAPIOutput(500, err, callback);
-        }
-        else {
-            console.log(data["Item"]);
+        } else {
+            console.log(data['Item']);
 
-            writeAPIOutput(200, data["Item"], callback)
+            writeAPIOutput(200, data['Item'], callback);
         }
     });
 }
 
+/**
+ * Handles a user POST request using the given doc client.
+ * Expects correct formatting of the body of the request to
+ * directly pass into the Database
+ *
+ * @param {object} docClient Initialized Document Client
+ * @param {object} event Event from the API invocation (JSON)
+ * @param {function} callback Callback for returning from the lambda
+ */
 function handleUserPost(docClient, event, callback) {
     let bodyJson = JSON.parse(event.body);
-    console.log("Body received: ", bodyJson);
+    console.log('Body received: ', bodyJson);
 
     let parameters = {
         TableName: 'User',
-        Item: bodyJson
+        Item: bodyJson,
     };
 
-    docClient.put(parameters, function (err, data) {
+    docClient.put(parameters, function(err, data) {
         if (err) {
-            console.log("Error", err);
+            console.log('Error', err);
 
-            writeAPIOutput(500, "Server error", callback);
-        }
-        else {
-            console.log("Success", data);
+            writeAPIOutput(500, 'Server error', callback);
+        } else {
+            console.log('Success', data);
 
-            writeAPIOutput(200, "Successfully created user!", callback);
+            writeAPIOutput(200, 'Successfully created user!', callback);
         }
     });
 }
 
+/**
+ * Main handler for a Lambda invocation.
+ * Event will contain the JSON of the API Gateway call, and callback
+ * must be called to return from invocation.
+ *
+ * @param {object} event JSON Information from API gateway
+ * @param {object} context Context info provided by Lambda
+ * @param {function} callback Callback to call to return from the function
+ */
 exports.handler = function(event, context, callback) {
-    console.log("Entered function handler...");
-    let dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' });
+    console.log('Entered function handler...');
     let docClient = new AWS.DynamoDB.DocumentClient();
 
     let method = event.httpMethod;
 
-    if (method === "POST") { //Create a new user in the database
-        console.log("Handling create user event");
+    if (method === 'POST') { // Create a new user
+        console.log('Handling create user event');
 
         handleUserPost(docClient, event, callback);
-    }
-    else if (method === "GET") { //Retrieve an existing user from the database
-        console.log("Handling get user event");
+    } else if (method === 'GET') { // Retrieve an existing user
+        console.log('Handling get user event');
 
         handleUserGet(docClient, event, callback);
-
-    }
-    else if (method === "PUT") {
-        console.log("Handling update user event");
+    } else if (method === 'PUT') { // Update existing user
+        console.log('Handling update user event');
         handleUserPut(docClient, event, callback);
-    }
-    else if (method === "DELETE") {
-        console.log("Handling delete user event");
+    } else if (method === 'DELETE') { // Delete existing user
+        console.log('Handling delete user event');
 
         handleUserDelete(docClient, event, callback);
     }
 };
 
+/**
+ * Write the status code and body message out API gateway
+ * @param {number} statCode Status code of the response
+ * @param {object} bodyMessage Message to return to the invoker
+ * @param {function} callback Callback from Lambda invocation
+ */
 function writeAPIOutput(statCode, bodyMessage, callback) {
     let response = {
-        statusCode : statCode,
-        body: JSON.stringify(bodyMessage)
+        statusCode: statCode,
+        body: JSON.stringify(bodyMessage),
     };
 
     callback(null, response);
--- lambdas/userLambdas/test/UserLambdaTest.js
+++ lambdas/userLambdas/test/UserLambdaTest.js
@@ -1,99 +1,122 @@
-import {handler} from "../UserLambda";
+import {handler} from '../UserLambda';
 
 let AWS = require('aws-sdk-mock');
 let assert = require('assert');
 
-AWS.mock('DynamoDB.DocumentClient', 'put', function (params, callback){
-    callback(null, "successfully posted item in database");
+AWS.mock('DynamoDB.DocumentClient', 'put', function(params, callback) {
+    callback(null, 'successfully posted item in database');
 });
 
-AWS.mock('DynamoDB.DocumentClient', 'delete', function (params, callback) {
-    callback(null, "successfully deleted item in database");
+AWS.mock('DynamoDB.DocumentClient', 'delete', function(params, callback) {
+    callback(null, 'successfully deleted item in database');
 });
 
-AWS.mock('DynamoDB.DocumentClient', 'update', function (params, callback) {
-    callback(null, "successfully updated item in database");
+AWS.mock('DynamoDB.DocumentClient', 'update', function(params, callback) {
+    callback(null, 'successfully updated item in database');
 });
 
-AWS.mock('DynamoDB.DocumentClient', 'get', function (params, callback) {
-    callback(null, {Item: {email: "testPerson@gmail.com", name: "Test Person"}});
+AWS.mock('DynamoDB.DocumentClient', 'get', function(params, callback) {
+    callback(null, {Item: {
+        email: 'testPerson@gmail.com',
+            name: 'Test Person'}});
 });
 
 
-describe("Test suite for User Lambda", function () {
-    it("Test get user from database", function () {
+describe('Test suite for User Lambda', function() {
+    it('Test get user from database', function() {
         let event = {
-            httpMethod: "GET",
-            body: "{}",
+            httpMethod: 'GET',
+            body: '{}',
             queryStringParameters: {
-                email: "joe.smitty@gmail.com"
-            }
+                email: 'joe.smitty@gmail.com',
+            },
         };
 
         handler(event, null, callback);
-        
+
+        /**
+         * Callback function for the Lambda event handler to verify
+         * JSON output
+         * @param {object} nothing Expect to be null
+         * @param {string} response Response from the API gateway invocation
+         */
         function callback(nothing, response) {
             assert.equal(response.statusCode, 200);
 
             let body = JSON.parse(response.body);
 
-            assert.equal(body.email, "testPerson@gmail.com");
-            assert.equal(body.name, "Test Person");
+            assert.equal(body.email, 'testPerson@gmail.com');
+            assert.equal(body.name, 'Test Person');
         }
-
     });
 
-    it("Test update user from database", function () {
+    it('Test update user from database', function() {
         let event = {
-            httpMethod: "PUT",
-            body: "{\"email\":\"joe.smitty@gmail.com\",\"name\":\"Joe Smitty\",\"rating\":\"5\"}"
+            httpMethod: 'PUT',
+            body: '{"email":"joe.smitty@gmail.com",' +
+            '"name":"Joe Smitty","rating":"5"}',
         };
 
         handler(event, null, callback);
 
+        /**
+         * Callback function for the Lambda event handler to verify
+         * JSON output
+         * @param {object} nothing Expect to be null
+         * @param {string} response Response from the API gateway invocation
+         */
         function callback(nothing, response) {
             assert.equal(response.statusCode, 200);
 
             let body = JSON.parse(response.body);
 
-            assert.equal(body, "Successfully updated user!");
+            assert.equal(body, 'Successfully updated user!');
         }
-
     });
 
-    it("Test delete user from database", function () {
+    it('Test delete user from database', function() {
         let event = {
-            httpMethod: "DELETE",
-            body: "{\"email\":\"joe.smitty@gmail.com\"}"
+            httpMethod: 'DELETE',
+            body: '{"email":"joe.smitty@gmail.com"}',
         };
 
         handler(event, null, callback);
 
+        /**
+         * Callback function for the Lambda event handler to verify
+         * JSON output
+         * @param {object} nothing Expect to be null
+         * @param {string} response Response from the API gateway invocation
+         */
         function callback(nothing, response) {
             assert.equal(response.statusCode, 200);
 
             let body = JSON.parse(response.body);
 
-            assert.equal(body, "Successfully deleted user!");
+            assert.equal(body, 'Successfully deleted user!');
         }
-
     });
 
-    it("Test post user into database", function () {
+    it('Test post user into database', function() {
         let event = {
-            httpMethod: "POST",
-            body: "{\"email\":\"joe.smitty@gmail.com\",\"name\":\"Joe Smitty\"}"
+            httpMethod: 'POST',
+            body: '{"email":"joe.smitty@gmail.com","name":"Joe Smitty"}',
         };
 
         handler(event, null, callback);
 
+        /**
+         * Callback function for the Lambda event handler to verify
+         * JSON output
+         * @param {object} nothing Expect to be null
+         * @param {string} response Response from the API gateway invocation
+         */
         function callback(nothing, response) {
             assert.equal(response.statusCode, 200);
 
             let body = JSON.parse(response.body);
 
-            assert.equal(body, "Successfully created user!");
+            assert.equal(body, 'Successfully created user!');
         }
-
     });
 });
